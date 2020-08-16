e46e70572f1454fdd413b19be1451b7dff16be67
David Daniels <dldaniels07@yahoo.com>
2020-08-15 21:17:08 -0600

    objective2lab2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+//Print Name: name + Age: age + State: state on same line
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
