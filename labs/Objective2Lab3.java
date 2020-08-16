8dd6f4aac6b1c46c0fe5b4827f64511e261ad78d
David Daniels <dldaniels07@yahoo.com>
2020-08-15 21:35:28 -0600

    objective2lab3

---
+++ labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+//assign number value to 3 values
+    num1 = 87;
+    num2 = 84;
+    num3 = 18;
+//find of average of num1, num2, and num3.
+//print calculated average
+    average = (num1 + num2 + num3) /3;
+    System.out.println(average);
+
+  }
+}
