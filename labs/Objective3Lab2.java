6aba103da04d9317645b2e119f38e2d97da97646
David Daniels <dldaniels07@yahoo.com>
2020-08-16 01:13:44 -0600

    objective3lab2

--- labs/Objective3Lab2.java
+++ labs/Objective3Lab2.java
@@ -8,6 +8,7 @@ public class Objective3Lab2 {
     side2 = 16.47;
 
     hypotenuse = Math.sqrt(side1 * side1 + side2 *side2);
-    System.out.println(hypotenuse);
+    System.out.println("The hypotenuse of a triangle with sides " +
+    side1 + " and " + side2 + " is " + hypotenuse);
   }
 }
