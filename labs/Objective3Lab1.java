142c5a50c1cc781dddba24d6f3ac583178c85c05
David Daniels <dldaniels07@yahoo.com>
2020-08-15 23:08:57 -0600

    objective3lab1

---
+++ labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    //declare double value for num1, num2, and num3.
+    num1 = 16;
+    num2 = 12;
+    num3 =7;
+    //compute average of thre numbers. Assign result to average variable
+    average = (num1 + num2 + num3) / 3;
+    //print average
+    System.out.println(average);
+  }
+}
