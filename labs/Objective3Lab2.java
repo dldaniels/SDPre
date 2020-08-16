2f4d73f2432709e45471787a9dc95258ceda5d7a
David Daniels <dldaniels07@yahoo.com>
2020-08-16 01:07:32 -0600

    objective3lab2

---
+++ labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2 {
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+    /*calculate hypotenuse of right triangle given 2 sides.
+    print hypotenuse*/
+
+    side1 = 12.33;
+    side2 = 16.47;
+
+    hypotenuse = Math.sqrt(side1 * side1 + side2 *side2);
+    System.out.println(hypotenuse);
+  }
+}
