a83bc6c3b839db9f984de8ca149110298f0aec92
David Daniels <dldaniels07@yahoo.com>
2020-08-17 19:38:08 -0600

    objective8Lab2

---
+++ labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int total = 0;
+    int i;
+
+    for (i = 1; i <= 20; i++) {
+      total += i;
+    }
+    System.out.println(total);
+  }
+}
